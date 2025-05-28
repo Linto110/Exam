cba791d1e4ff5d94ffd3581d4f22b66304aff214
LINTO EMMANUEL <lintoemmanuel8@gmail.com>
2025-05-28 18:32:35 +0530

    Update directories.sh

--- directories.sh
+++ directories.sh
@@ -1,3 +1,4 @@
+#take all file and all from the current directory checks if its a directory not if yes then prints its name 
 for f in *
 do
 	if [ -d "$f" ];then #$f is single word filename ""is used around if space in filenames
